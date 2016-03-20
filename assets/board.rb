9dba9afc9c7e4a13b5507945ce2dd1fce2399ee9
MrRoboman <rkayson@gmail.com>
2016-03-20 13:15:08 -0700

    changed Board.build_board to Board.build_grid

--- assets/board.rb
+++ assets/board.rb
@@ -1,5 +1,9 @@
 class Board
-  def self.build_board
+  def self.build_grid
     Array.new(8) { Array.new(8) { NullPiece.instance} }
+  end
+
+  def initialize
+    grid = Board.build_grid
   end
 end
